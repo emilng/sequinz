8bd1a34234ae0491a69037c6a79ab18708c9df0e
Emil Ng <github@alphabeticaldisorder.com>
2020-11-20 17:18:55 -0500

    update for writing future tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import { render } from "@testing-library/svelte";
 import App from "./App";
 
-test("renders learn svelte link", () => {
-  const { getByText } = render(App);
-  const linkElement = getByText(/learn svelte/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+	it('has a placeholder test', () => {});
 });
